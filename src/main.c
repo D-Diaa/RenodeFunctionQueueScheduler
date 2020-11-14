d7cee2c88cb561b9d076e3ee886a076e4f543015
D-Diaa <a.diaa@aucegypt.edu>
2020-11-14 15:47:00 +0200

    scheduling and basic example

---
+++ src/main.c
@@ -0,0 +1,89 @@
+#include "task_queue.h"
+#include "renode_helpers.h"
+
+static const uint16_t MAX_SIZE = 16;
+static uint8_t msg[] = "IDLE !!\n";
+static struct task_queue main_queue;
+static struct task_queue delayed_queue;
+
+// Enqueue task in the main queue with a certain priorty
+static void queue_task(fptr f, int prio)
+{
+		enqueue(&main_queue, f, prio, 0);
+}
+
+// Dispatch a task and run it if available, otherwise display 'idle'
+static void dispatch()
+{
+		// handle tasks in delayed queue
+		if(delayed_queue.cur_sz>0)
+		{
+			// decrement delays
+			decrement_all(&delayed_queue);
+			// pop those ones that are ready from the delayed to the main
+			push_all_ready(&delayed_queue, &main_queue);
+		}
+		// dispatch tasks if available, else display ("idle")
+		if(main_queue.cur_sz>0) dequeue(&main_queue).f();
+		else sendUART(msg, sizeof(msg));
+}
+
+// Allows rerunning a task (acts mainly as a conditional enqueuer)
+static void rerun (fptr f, int prio, int delay)
+{
+		// Enqueue to the main if the task is not delayed, otherwise put in the delayed queue
+		if(delay==0) enqueue(&main_queue, f, prio, delay);
+		else enqueue(&delayed_queue, f, prio, delay);
+}
+
+// Initialize peripherals and queues
+static void init()
+{
+		hardware_init();
+		main_queue = q_init(MAX_SIZE);
+		delayed_queue = q_init(MAX_SIZE);	
+}
+
+// Example tasks
+
+static void f1()
+{
+	uint8_t tst[] = "ONE!!! \n";
+	sendUART(tst, sizeof(tst));
+	rerun(&f1, 1, 5);
+}
+static void f2()
+{
+	uint8_t tst[] = "TWO!!! \n";
+	sendUART(tst, sizeof(tst));
+	rerun(&f2, 2, 10);
+}
+static void f3()
+{
+	uint8_t tst[] = "THREE!!! \n";
+	sendUART(tst, sizeof(tst));
+}
+static void f4()
+{
+	uint8_t tst[] = "FOUR!!! \n";
+	sendUART(tst, sizeof(tst));
+}
+
+// Main program
+int main()
+{	
+		init();
+		queue_task(&f2, 2);
+		queue_task(&f1, 1);
+		queue_task(&f3, 3);
+		queue_task(&f4, 4);
+
+	  while(1)
+		{
+				if(timerFlag && !stopFlag)
+				{
+						dispatch();
+					  timerFlag = 0;
+				}
+		}
+}
