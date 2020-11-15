8dd12f18234d9f0a0188695c95c21cc1dede875c
Ismail <isen.shaheen@outlook.com>
2020-11-15 08:12:14 +0200

    formatting

--- src/main.c
+++ src/main.c
@@ -5,14 +5,13 @@ static const uint16_t MAX_SIZE = 20;
 static uint8_t msg[] = "IDLE !!\n";
 static struct task_queue main_queue;
 static struct task_queue delayed_queue;
-
 static volatile uint16_t skipped_cycles = 0;
 static volatile char timerFlag = 0;
 
-void SysTick_Handler(void);
+//void SysTick_Handler(void);
 
-
-void SysTick_Handler(void) {
+void SysTick_Handler(void)
+{
 	timerFlag = 1;
 	skipped_cycles++;
 }
@@ -20,45 +19,43 @@ void SysTick_Handler(void) {
 // Enqueue task in the main queue with a certain priorty
 static void queue_task(fptr f, uint16_t prio)
 {
-		enqueue(&main_queue, f, prio, 0);
+	enqueue(&main_queue, f, prio, 0);
 }
 
 // Dispatch a task and run it if available, otherwise display 'idle'
 static void dispatch()
 {
-		// handle tasks in delayed queue
-		if(delayed_queue.cur_sz>0)
-		{
-			// decrement delays
-			decrement_all(&delayed_queue, skipped_cycles);
-			// pop those ones that are ready from the delayed to the main
-			push_all_ready(&delayed_queue, &main_queue);
-		}
-		// New dispacthed --> no skipped cycles for the rest
-		skipped_cycles = 0;
-		// dispatch tasks if available, else display ("idle")
-		if(main_queue.cur_sz>0) dequeue(&main_queue).f();
-		else sendUART(msg, sizeof(msg));
+	// handle tasks in delayed queue
+	if (delayed_queue.cur_sz > 0) {
+		// decrement delays
+		decrement_all(&delayed_queue, skipped_cycles);
+		// pop those ones that are ready from the delayed to the main
+		push_all_ready(&delayed_queue, &main_queue);
+	}
+	// New dispacthed --> no skipped cycles for the rest
+	skipped_cycles = 0;
+	// dispatch tasks if available, else display ("idle")
+	if(main_queue.cur_sz > 0) dequeue(&main_queue).f();
+	else sendUART(msg, sizeof(msg));
 }
 
 // Allows rerunning a task (acts mainly as a conditional enqueuer)
 static void rerun (fptr f, uint16_t prio, uint16_t delay)
 {
-		// Enqueue to the main if the task is not delayed, otherwise put in the delayed queue
-		if(delay==0) enqueue(&main_queue, f, prio, delay);
-		else enqueue(&delayed_queue, f, prio, delay);
+	// Enqueue to the main if the task is not delayed, otherwise put in the delayed queue
+	if(delay == 0) enqueue(&main_queue, f, prio, delay);
+	else enqueue(&delayed_queue, f, prio, delay);
 }
 
 // Initialize peripherals and queues
 static void init()
 {
-		hardware_init();
-		main_queue = q_init(MAX_SIZE);
-		delayed_queue = q_init(MAX_SIZE);	
+	hardware_init();
+	main_queue = q_init(MAX_SIZE);
+	delayed_queue = q_init(MAX_SIZE);	
 }
 
 // Example tasks
-
 static void f1()
 {
 	uint8_t tst[] = "ONE!!! \n";
@@ -76,7 +73,8 @@ static void f3()
 {
 	uint8_t tst[] = "THREE!!! \n";
 	sendUART(tst, sizeof(tst));
-	for(j=0; j<2000; ++j)for(i=0; i<10000; ++i);
+	for(j = 0; j < 2000; ++j)
+		for(i = 0; i < 10000; ++i);
 }
 static void f4()
 {
@@ -87,18 +85,16 @@ static void f4()
 // Main program
 int main()
 {	
-		init();
-		queue_task(&f2, 2);
-		queue_task(&f1, 1);
-		queue_task(&f3, 3);
-		queue_task(&f4, 4);
+	init();
+	queue_task(&f2, 2);
+	queue_task(&f1, 1);
+	queue_task(&f3, 3);
+	queue_task(&f4, 4);
 
-	  while(1)
-		{
-				if(timerFlag && !stopFlag)
-				{
-						dispatch();
-					  timerFlag = 0;
-				}
+	while(1) {
+		if(timerFlag && !stopFlag) {
+			dispatch();
+			timerFlag = 0;
 		}
+	}
 }
--- src/task_queue.h
+++ src/task_queue.h
@@ -1,4 +1,4 @@
-#include  <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 
@@ -10,17 +10,17 @@
 typedef void (*fptr)(void);
 
 // Task object
-struct task{
-		fptr f;
-		uint16_t prio;
-		uint16_t delay;
+struct task {
+	fptr f;
+	uint16_t prio;
+	uint16_t delay;
 };
 
 // Task queue
-struct task_queue{
-    uint16_t cur_sz;
-		uint16_t max_sz;
-    struct task * tasks;
+struct task_queue {
+	uint16_t cur_sz;
+	uint16_t max_sz;
+	struct task * tasks;
 };
 
 // Function declarations
@@ -37,89 +37,88 @@ int compare(struct task a, struct task b);
 // Helper: less than operator for tasks (treated as pair<delay, prio>)
 int compare(struct task a, struct task b)
 {
-	if(a.delay<b.delay) return 1;
-	return (a.delay==b.delay) && (a.prio<b.prio);
+	if(a.delay < b.delay) return 1;
+	return (a.delay == b.delay) && (a.prio < b.prio);
 }
 
 // Helper: Swap task references
-void swap(struct task *task1, struct task *task2) {
-		struct task temp = *task1;
-		*task1 = *task2;
-		*task2 = temp;
+void swap(struct task *task1, struct task *task2)
+{
+	struct task temp = *task1;
+	*task1 = *task2;
+	*task2 = temp;
 }
 
 // Task queue constructor
 struct task_queue q_init(uint16_t size)
 {
-		struct task_queue q;
-		q.cur_sz = 0;
-		q.max_sz = size;
-		q.tasks = malloc(size*sizeof(struct task));
-		return q;
+	struct task_queue q;
+	q.cur_sz = 0;
+	q.max_sz = size;
+	q.tasks = malloc(size * sizeof(struct task));
+	return q;
 }
 
 // Enqueue function
 void enqueue(struct task_queue* q, fptr f, uint16_t prio, uint16_t delay)
 {
-		// Create new task and call the private enqueue function
-		struct task new_task = {f, prio, delay};
-		_enqueue(q, new_task);
+	// Create new task and call the private enqueue function
+	struct task new_task = {f, prio, delay};
+	_enqueue(q, new_task);
 }
 
 // Enqueue function
 void _enqueue(struct task_queue* q, struct task new_task)
 {
-		// Check for queue limits
-		if (q->cur_sz == q->max_sz -1)
-		{
-				printf("ERROR! - Queue is full");
-				return;
-		}
-		// put at end of the queue
-		q->tasks[q->cur_sz++]=new_task;
-		
-		// Go up the tree till it can't overthrow its parent
-		int i = q->cur_sz-1;
-		while (i != 0 && compare(q->tasks[i], q->tasks[p(i)]))
-		{
-				swap( & q->tasks[p(i)], & q->tasks[i]);
-				i = p(i);
-		}
+	// Check for queue limits
+	if (q->cur_sz == q->max_sz - 1) {
+		printf("ERROR! - Queue is full"); //TODO: redirect error stream
+		return;
+	}
+	// put at end of the queue
+	q->tasks[q->cur_sz++]=new_task;
+	
+	// Go up the tree till it can't overthrow its parent
+	int i = q->cur_sz-1;
+	while (i != 0 && compare(q->tasks[i], q->tasks[p(i)])) {
+		swap(&q->tasks[p(i)], &q->tasks[i]);
+		i = p(i);
+	}
 }
 
 // Max heap algorithm for dequeuing
-void min_heap(struct task_queue* q, int i) {
-		int small = i;
-		int left = lf(i), right = rt(i);
-		// get the minimum (left or right or this node)
-		if(left<=q->cur_sz && compare(q->tasks[left],q->tasks[small])) small = left;
-		if(right<=q->cur_sz && compare(q->tasks[right],q->tasks[small])) small = right;
-		// if this node is the maximum then left, right and this subtree are heapified
-		if (small==i) return;
-		// else put the small on top and heapify the affected tree
-		swap(&q->tasks[small], &q->tasks[i]);
-		min_heap(q, small);
+void min_heap(struct task_queue* q, int i)
+{
+	int small = i;
+	int left = lf(i), right = rt(i);
+	// get the minimum (left or right or this node)
+	if(left <= q->cur_sz && compare(q->tasks[left], q->tasks[small])) small = left;
+	if(right <= q->cur_sz && compare(q->tasks[right], q->tasks[small])) small = right;
+	// if this node is the maximum then left, right and this subtree are heapified
+	if (small == i) return;
+	// else put the small on top and heapify the affected tree
+	swap(&q->tasks[small], &q->tasks[i]);
+	min_heap(q, small);
 }
 
 // Dequeue function
 struct task dequeue(struct task_queue* q)
 {
-		struct task ret = {0, 0, 0};
-		// Check for queue limits
-		if (q->cur_sz ==0)
-		{
-				printf("ERROR! - Queue is empty");
-				return ret;
-		}
-		// Take task reference and replace its reference with the lasr task in the queue
-		ret = q->tasks[0];
-		q->tasks[0] = q->tasks[q->cur_sz-1];
-		// Decrease queue size
-		q->cur_sz--;
-		// Put the last task (now at the top of the queue) in its correct place
-		min_heap(q, 0);
-		// return the desired task
+	struct task ret = {0, 0, 0};
+	// Check for queue limits
+	if (q->cur_sz == 0) {
+		printf("ERROR! - Queue is empty"); //TODO: redirect error stream
 		return ret;
+	}
+	// Take task reference and replace its reference with the lasr task in the queue
+	ret = q->tasks[0];
+	q->tasks[0] = q->tasks[q->cur_sz-1];
+	// Decrease queue size
+	q->cur_sz--;
+	// Put the last task (now at the top of the queue) in its correct place
+	min_heap(q, 0);
+	// return the desired task
+	return ret;
 }
 
 // Decrements all the delays in a certain queue (used with the delayed queue)
