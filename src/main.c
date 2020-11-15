0c86a9333d19084978ff8aaae424afb8ec63e7d0
Ismail <isen.shaheen@outlook.com>
2020-11-16 00:35:54 +0200

    formatting again :(

--- src/main.c
+++ src/main.c
@@ -11,10 +11,8 @@ static struct task_queue delayed_queue;
 static volatile uint16_t skipped_cycles = 0;
 static volatile char timerFlag = 0;
 
-void SysTick_Handler(void);
-
-
-void SysTick_Handler(void) {
+void SysTick_Handler(void)
+{
 	timerFlag = 1;
 	skipped_cycles++;
 }
@@ -22,40 +20,40 @@ void SysTick_Handler(void) {
 // Enqueue task in the main queue with a certain priorty
 static void queue_task(fptr f, uint16_t prio)
 {
-		enqueue(&main_queue, f, prio, 0);
+	enqueue(&main_queue, f, prio, 0);
 }
 
 // Dispatch a task and run it if available, otherwise display 'idle'
 static void dispatch()
 {
-		// handle tasks in delayed queue
-		// decrement delays
-		decrement_all(&delayed_queue, skipped_cycles);
-		// pop those ones that are ready from the delayed to the main
-		push_all_ready(&delayed_queue, &main_queue);
-		// New dispacthed --> no skipped cycles for the rest
-		skipped_cycles = 0;
-		// dispatch tasks if available, else display ("idle")
-		if(main_queue.cur_sz>0) dequeue(&main_queue).f();
-		else sendUART(msg, sizeof(msg));
+	// handle tasks in delayed queue
+	// decrement delays
+	decrement_all(&delayed_queue, skipped_cycles);
+	// pop those ones that are ready from the delayed to the main
+	push_all_ready(&delayed_queue, &main_queue);
+	// New dispacthed --> no skipped cycles for the rest
+	skipped_cycles = 0;
+	// dispatch tasks if available, else display ("idle")
+	if(main_queue.cur_sz>0) dequeue(&main_queue).f();
+	else sendUART(msg, sizeof(msg));
 }
 
 // Allows rerunning a task (acts mainly as a conditional enqueuer)
 static void rerun (fptr f, uint16_t prio, uint16_t delay)
 {
-		// Enqueue to the main if the task is not delayed, otherwise put in the delayed queue
-		if(delay==0) enqueue(&main_queue, f, prio, delay);
-		else enqueue(&delayed_queue, f, prio, delay);
+	// Enqueue to the main if the task is not delayed, otherwise put in the delayed queue
+	if(delay == 0) enqueue(&main_queue, f, prio, delay);
+	else enqueue(&delayed_queue, f, prio, delay);
 }
 
 // Initialize peripherals and queues
 static inline void init()
 {
-		// initialize emulator hardware registers
-		hardware_init();
-		// initialize queues
-		q_init(&main_queue,MAX_SIZE);
-		q_init(&delayed_queue, MAX_SIZE);	
+	// initialize emulator hardware registers
+	hardware_init();
+	// initialize queues
+	q_init(&main_queue,MAX_SIZE);
+	q_init(&delayed_queue, MAX_SIZE);	
 }
 
 // Example tasks
@@ -89,21 +87,19 @@ static void f4()
 // Main program
 int main()
 {	
-		init();
-		queue_task(&f2, 2);
-		queue_task(&f1, 1);
-		queue_task(&f3, 3);
-		queue_task(&f4, 4);
+	init();
+	queue_task(&f2, 2);
+	queue_task(&f1, 1);
+	queue_task(&f3, 3);
+	queue_task(&f4, 4);
 
-	  while(1)
-		{
-				if(timerFlag)
-				{
-						// dispatch only if manual delay is not invoked
-						if(!stopFlag)	dispatch();
-						// feedback on manual delay
-						else sendUART(delay_msg, sizeof(delay_msg)); 		
-					timerFlag = 0;  
-				}
+	while(1) {
+		if(timerFlag) {
+			// dispatch only if manual delay is not invoked
+			if(!stopFlag) dispatch();
+			// feedback on manual delay
+			else sendUART(delay_msg, sizeof(delay_msg)); 		
+			timerFlag = 0;  
 		}
+	}
 }
